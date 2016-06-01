4e056e537d4349a62958b581e47707b16f4198d8
Jaime Francisco Duarte Pinzón <jaimeduarte2411@hotmail.com>
2016-05-31 22:57:59 -0600

    Activities

---
+++ app/src/main/java/com/jimmy/uabcs/bibliouabcs/views/AboutActivity.java
@@ -0,0 +1,15 @@
+package com.jimmy.uabcs.bibliouabcs.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jimmy.uabcs.bibliouabcs.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
